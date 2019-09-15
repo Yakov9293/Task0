c7e315fbdff41dad6a64524ae80f3b629d12f41e
Yakov9293 <yakov226805@yandex.ru>
2019-09-15 19:29:58 +0300

    All commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         int exit = 1;
-        while(exit == 1) {
+        while (exit == 1) {
             Task task = new Task();
             task.startGame();
             task.chooseDifficult();
@@ -13,7 +13,20 @@ public class Main {
             task.gameProcess();
             System.out.println("Play again? 1 - YES! 0 - No, I'm a boring poodle");
             Scanner scanner = new Scanner(System.in);
-            exit = scanner.nextInt();
+            exit = 2;
+            while (exit != 0 && exit != 1) {
+                try {
+                    exit = scanner.nextInt();
+                } catch (Exception ex) {
+                    exit = 2;
+                    scanner.next();
+                }
+                if (exit != 0 && exit != 1) {
+                    System.out.println("Incorrect input");
+                }
+            }
+
+
         }
     }
 }
